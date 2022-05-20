cd34c6454a29a64c8f17c638c72b7dc59a0c2750
Trevor Rapp <trrapp@gmail.com>
2022-05-19 20:32:30 -0600

    fixes the issue where the number disappears by creating a listener that will check the elements scrollwidth against the clientwidth and reduce the font accordingly :fire::alien::fire::alien::fire:

---
+++ font-size.js
@@ -0,0 +1,51 @@
+(() => {
+
+  window.addEventListener('load', () => {
+
+    console.log('entered font size js')
+  const inputElement = document.getElementById('amount');
+  let fontSize = 150;
+
+
+  inputElement.addEventListener('keyup', () => {
+
+    function setFontSize (value) {
+      console.log('setFontSize firing, value is ' + value)
+      inputElement.style.fontSize = `${value}px` ;
+    }
+  
+    function testFontSize (el) {
+      console.log("testFontSize firing, el is " + el)
+      return el.scrollWidth > el.clientWidth;
+    }
+  
+    function triggerFontComparison () {
+      console.log("triggerFontComparision is firing")
+      if (testFontSize(inputElement)) {
+        console.log('entering testFontSize if statement')
+        while (testFontSize(inputElement)) {
+          console.log('entering first while loop')
+          fontSize--; 
+          setFontSize(fontSize);
+        }
+      } else {
+        console.log('entering else statement')
+        fontSize = 150;
+        setFontSize(fontSize)
+        while (testFontSize(inputElement)){
+          console.log('entering second while loop')
+          fontSize--;
+          setFontSize(fontSize);
+      }
+    }
+    }
+
+    triggerFontComparison()
+  })
+
+
+  })
+
+  
+
+})();
