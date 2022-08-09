ba1eff1bd68d7db3707ac0e1428e94d67c58a0fd
Trevor Rapp <trrapp@gmail.com>
2022-08-08 23:16:47 -0600

    able to get the function to turn back, it wasn't ever getting turned back to true

--- accordion.js
+++ accordion.js
@@ -2,7 +2,6 @@
     // classNames returns an Object
     const hebrewWords = document.getElementsByClassName('hebrew');
     const rightAligned = document.getElementsByClassName('right-align');
-    // Id's
     const topBar = document.getElementById('english-side-bar');
     const bottomBar = document.getElementById('hebrew-side-bar');
 
@@ -10,38 +9,39 @@
 
     function toggleHidden(param) {
         for (let [key, value] of Object.entries(param)) {
-            value.classList.toggle('hidden')
+            value.classList.toggle('hidden');
         }
     }
 
-    function toggleAccordion(param) {
-        param.classList.toggle('accordion-bar-hover')
-    }
-
     function changeVisibilityOnSize(obj) {
-        let x = obj
         if (window.innerWidth < 1600) {
-            toggleHidden(x)
+            toggleHidden(obj);
         } else if (window.innerWidth > 1600) {
-            toggleHidden(x)
+            toggleHidden(obj);
         }
     }
 
     window.addEventListener('resize', () => {
-        console.log(`window event listener ${timeout}`)
-        if (!timeout) return;
+
+        // if (!timeout) return;
         
-        timeout = false;
+        // timeout = false;
 
         setTimeout(()=> {
             changeVisibilityOnSize(hebrewWords);
             changeVisibilityOnSize(rightAligned);
-        }, 300)
-    })
+            
+        }, 600);
 
-    topBar.addEventListener('click', toggleHidden);
-    topBar.addEventListener('mouseover', toggleHidden);
-    bottomBar.addEventListener('click', toggleAccordion);
-    bottomBar.addEventListener('mouseover', toggleAccordion)
+        // timeout = true;
+    });
 
+    function toggleAccordion(obj) {
+        obj.classList.toggle('accordion-bar-hover');
+    }
+
+    topBar.addEventListener('click', toggleAccordion(topBar));
+    topBar.addEventListener('mouseover', toggleAccordion(topBar));
+    bottomBar.addEventListener('click', toggleAccordion(bottomBar));
+    bottomBar.addEventListener('mouseover', toggleAccordion(bottomBar));
 })();
